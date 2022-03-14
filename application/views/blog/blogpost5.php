6a386fc88fe2ad7b9e6f307fe21d889d2a4ad100
Awuah Maxwell <nanayawfixing@gmail.com>
2022-03-14 04:10:39 +0000

    ammend

--- application/views/blog/blogpost5.php
+++ application/views/blog/blogpost5.php
@@ -1,9 +1,9 @@
 <?php
-require('config.php')
+require_once('config.php')
 ?>
 <!-- head here -->
 <?php
-require(ROOT_PATH . '/includes/head.php')
+require_once('/includes/head.php')
 ?>
 <title>My blog post</title>
 </head>
@@ -11,30 +11,32 @@ require(ROOT_PATH . '/includes/head.php')
 <body>
     <!-- navbar below -->
     <?php
-    require(ROOT_PATH . '/includes/navbar.php')
+    require_once('/includes/navbar.php')
     ?>
     <!-- header below -->
     <?php
-    require(ROOT_PATH . '/includes/header.php')
+    require_once('/includes/header.php')
     ?>
     <div class="main-wrapper min-vh-100 pt-5">
         <article class="blog-post px-3 py-5 p-md-5">
             <?php foreach ($posts as $post) { ?>
+
                 <div class="container">
                     <div class="blog-post-header">
-                        <h2 class="title mb-2">Why Every Developer Should Have A Blog</h2>
+                        <h2 class="title mb-2"><?=  $post['title'] ?></h2>
                         <div class="meta mb-3"><span class="date">Published 3 months ago</span><span class="time">5 min read</span><span class="comment"><a href="#">4 comments</a></span></div>
                     </div>
 
                     <div class="blog-post-body">
-                        <img class="img-fluid" src="assets/images/blog/blog-post-banner.jpg" alt="image">
-                        <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. </p>
+                        <img class="img-fluid" src="<?php echo BASE_URL . 'application/views/blog/static/images/' . $post['image']; ?>" alt="image">
+                        <p><?=  $post['body']  ?> </p>
                     </div>
                 </div>
             <?php } ?>
         </article>
+
         <?php
-        require(ROOT_PATH . '/includes/footer.php')
+        require_once('/includes/footer.php')
         ?>
     </div>
 
--- application/views/blog/includes/head.php
+++ application/views/blog/includes/head.php
@@ -8,5 +8,5 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="<?php echo BASE_URL . 'application/views/blog/static/theme-1.css'?>">
     <link rel="stylesheet" href="<?php echo BASE_URL . 'application/views/blog/static/mytheme.css'?>">
-    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/styles/monokai-sublime.min.css">
+    <!-- <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/highlight.js/9.14.2/styles/monokai-sublime.min.css"> -->
     <script defer src="https://use.fontawesome.com/releases/v5.7.1/js/all.js" integrity="sha384-eVEQC9zshBn0rFj4+TU78eNA19HMNigMviK/PU/FFjLXqa/GKPgX58rvt5Z8PLs7" crossorigin="anonymous"></script>
